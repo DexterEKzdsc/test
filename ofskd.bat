7f2f769f232548bebed48282d7a84d9b14fedf72
DexterEKzdsc <68162086+DexterEKzdsc@users.noreply.github.com>
2020-07-11 19:42:55 +0200

    prr

---
+++ ofskd.bat
@@ -0,0 +1,14 @@
+
+powershell -Command "& {Get-AppxPackage -AllUsers xbox | Remove-AppxPackage}" >NUL 2>NUL
+sc stop XblAuthManager >NUL 2>NUL
+sc stop XblGameSave >NUL 2>NUL
+sc stop XboxNetApiSvc >NUL 2>NUL
+sc stop XboxGipSvc >NUL 2>NUL
+sc delete XblAuthManager >NUL 2>NUL
+sc delete XblGameSave >NUL 2>NUL
+sc delete XboxNetApiSvc >NUL 2>NUL
+sc delete XboxGipSvc >NUL 2>NUL
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /f >NUL 2>NUL
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /disable >NUL 2>NUL
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTaskLogon" /disable >NUL 2>NUL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f >NUL 2>NUL
